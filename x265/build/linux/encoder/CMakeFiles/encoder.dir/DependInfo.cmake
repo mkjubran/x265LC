736aa30d6988bfa612ccfdcff1ee80b41acc228a
mkjubran <mjubran@birzeit.edu>
2019-09-17 08:05:47 +0000

    Ver0.1

---
+++ x265/build/linux/encoder/CMakeFiles/encoder.dir/DependInfo.cmake
@@ -0,0 +1,54 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/data1/Mohd_All/LearnableCoding/FFMPEGLC/x265/source/encoder/analysis.cpp" "/data1/Mohd_All/LearnableCoding/FFMPEGLC/x265/build/linux/encoder/CMakeFiles/encoder.dir/analysis.cpp.o"
+  "/data1/Mohd_All/LearnableCoding/FFMPEGLC/x265/source/encoder/api.cpp" "/data1/Mohd_All/LearnableCoding/FFMPEGLC/x265/build/linux/encoder/CMakeFiles/encoder.dir/api.cpp.o"
+  "/data1/Mohd_All/LearnableCoding/FFMPEGLC/x265/source/encoder/bitcost.cpp" "/data1/Mohd_All/LearnableCoding/FFMPEGLC/x265/build/linux/encoder/CMakeFiles/encoder.dir/bitcost.cpp.o"
+  "/data1/Mohd_All/LearnableCoding/FFMPEGLC/x265/source/encoder/dpb.cpp" "/data1/Mohd_All/LearnableCoding/FFMPEGLC/x265/build/linux/encoder/CMakeFiles/encoder.dir/dpb.cpp.o"
+  "/data1/Mohd_All/LearnableCoding/FFMPEGLC/x265/source/encoder/encoder.cpp" "/data1/Mohd_All/LearnableCoding/FFMPEGLC/x265/build/linux/encoder/CMakeFiles/encoder.dir/encoder.cpp.o"
+  "/data1/Mohd_All/LearnableCoding/FFMPEGLC/x265/source/encoder/entropy.cpp" "/data1/Mohd_All/LearnableCoding/FFMPEGLC/x265/build/linux/encoder/CMakeFiles/encoder.dir/entropy.cpp.o"
+  "/data1/Mohd_All/LearnableCoding/FFMPEGLC/x265/source/encoder/frameencoder.cpp" "/data1/Mohd_All/LearnableCoding/FFMPEGLC/x265/build/linux/encoder/CMakeFiles/encoder.dir/frameencoder.cpp.o"
+  "/data1/Mohd_All/LearnableCoding/FFMPEGLC/x265/source/encoder/framefilter.cpp" "/data1/Mohd_All/LearnableCoding/FFMPEGLC/x265/build/linux/encoder/CMakeFiles/encoder.dir/framefilter.cpp.o"
+  "/data1/Mohd_All/LearnableCoding/FFMPEGLC/x265/source/encoder/level.cpp" "/data1/Mohd_All/LearnableCoding/FFMPEGLC/x265/build/linux/encoder/CMakeFiles/encoder.dir/level.cpp.o"
+  "/data1/Mohd_All/LearnableCoding/FFMPEGLC/x265/source/encoder/motion.cpp" "/data1/Mohd_All/LearnableCoding/FFMPEGLC/x265/build/linux/encoder/CMakeFiles/encoder.dir/motion.cpp.o"
+  "/data1/Mohd_All/LearnableCoding/FFMPEGLC/x265/source/encoder/nal.cpp" "/data1/Mohd_All/LearnableCoding/FFMPEGLC/x265/build/linux/encoder/CMakeFiles/encoder.dir/nal.cpp.o"
+  "/data1/Mohd_All/LearnableCoding/FFMPEGLC/x265/source/encoder/ratecontrol.cpp" "/data1/Mohd_All/LearnableCoding/FFMPEGLC/x265/build/linux/encoder/CMakeFiles/encoder.dir/ratecontrol.cpp.o"
+  "/data1/Mohd_All/LearnableCoding/FFMPEGLC/x265/source/encoder/reference.cpp" "/data1/Mohd_All/LearnableCoding/FFMPEGLC/x265/build/linux/encoder/CMakeFiles/encoder.dir/reference.cpp.o"
+  "/data1/Mohd_All/LearnableCoding/FFMPEGLC/x265/source/encoder/sao.cpp" "/data1/Mohd_All/LearnableCoding/FFMPEGLC/x265/build/linux/encoder/CMakeFiles/encoder.dir/sao.cpp.o"
+  "/data1/Mohd_All/LearnableCoding/FFMPEGLC/x265/source/encoder/search.cpp" "/data1/Mohd_All/LearnableCoding/FFMPEGLC/x265/build/linux/encoder/CMakeFiles/encoder.dir/search.cpp.o"
+  "/data1/Mohd_All/LearnableCoding/FFMPEGLC/x265/source/encoder/sei.cpp" "/data1/Mohd_All/LearnableCoding/FFMPEGLC/x265/build/linux/encoder/CMakeFiles/encoder.dir/sei.cpp.o"
+  "/data1/Mohd_All/LearnableCoding/FFMPEGLC/x265/source/encoder/slicetype.cpp" "/data1/Mohd_All/LearnableCoding/FFMPEGLC/x265/build/linux/encoder/CMakeFiles/encoder.dir/slicetype.cpp.o"
+  "/data1/Mohd_All/LearnableCoding/FFMPEGLC/x265/source/encoder/weightPrediction.cpp" "/data1/Mohd_All/LearnableCoding/FFMPEGLC/x265/build/linux/encoder/CMakeFiles/encoder.dir/weightPrediction.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "EXPORT_C_API=1"
+  "HAVE_INT_TYPES_H=1"
+  "HAVE_LIBNUMA"
+  "HIGH_BIT_DEPTH=0"
+  "X265_ARCH_X86=1"
+  "X265_DEPTH=8"
+  "X265_NS=x265"
+  "X86_64=1"
+  "__STDC_LIMIT_MACROS=1"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/data1/Mohd_All/LearnableCoding/FFMPEGLC/x265/source/."
+  "/data1/Mohd_All/LearnableCoding/FFMPEGLC/x265/source/common"
+  "/data1/Mohd_All/LearnableCoding/FFMPEGLC/x265/source/encoder"
+  "."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
